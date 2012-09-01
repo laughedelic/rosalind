79c8defb49b0d9132e99e9578fa57cb2d1ed697a
Alexey Alekhin <alexey.alekhin@me.com>
2012-09-02 00:07:13 +0400

    initial commit with two solutions

---
+++ readme.markdown
@@ -0,0 +1,2 @@
+This is my repo with solutions of problems from the [Rosalind](http://rosalind.info/) project. 
+I just want to practice in Scala and learn something about bioinformatics problems and algorythms.
---
+++ rosalind-solutions.scala
@@ -0,0 +1,22 @@
+// there are solutions to problems from Rosalind project (see links)
+
+
+def updMap[K,V](m: Map[K,V], key: K, f: V => V): Map[K,V] =
+	if(m contains key) m + (key -> f(m(key))) else m
+
+
+// http://rosalind.info/problems/acgu/
+def acgu(rna: String): Map[Char, Int] =
+	rna.foldLeft(Map('A' -> 0, 'C' -> 0, 'G' -> 0, 'U' -> 0))( (acc, x) => updMap(acc, x, (_:Int) + 1) )
+//////////////////////////////////////
+
+
+// http://rosalind.info/problems/revc/
+def revc(dna: String): String = 
+	dna.reverse.map(b => b match {
+		case 'A' => 'T'
+		case 'T' => 'A'
+		case 'C' => 'G'
+		case 'G' => 'C'
+	})
+/////////////////////////////////////
