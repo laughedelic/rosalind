19edcec7c29e967585b93aaf4563b976002142c8
Alexey Alekhin <alexey.alekhin@me.com>
2012-09-02 16:49:52 +0400

    some more formatting

--- rosalind-solutions.scala
+++ rosalind-solutions.scala
@@ -8,13 +8,21 @@ def updMap[K,V](m: Map[K,V], key: K, f: V => V): Map[K,V] =
 // Problems:
 
 //// Counting Bases: http://rosalind.info/problems/acgu/ ///////////////////////
-def acgu(rna: String): Map[Char, Int] =
-    rna.foldLeft(Map('A' -> 0, 'C' -> 0, 'G' -> 0, 'U' -> 0))( 
+// Given: An RNA string s of length at most 1000 nt.
+// Return: Four integers corresponding to the number of times that the symbols A, C, G, and U occur in s.
+
+def countBases(rna: String): Map[Char, Int] =
+	rna.foldLeft(Map('A' -> 0, 'C' -> 0, 'G' -> 0, 'U' -> 0))(
         (acc, x) => updMap(acc, x, (_:Int) + 1) )
+
+def acgu(rna: String): String = countBases(rna).values.mkString(" ")
 ////////////////////////////////////////////////////////////////////////////////
 
 
 //// Reverse Complement: http://rosalind.info/problems/revc/ ///////////////////
+// Given: A DNA string s of length at most 1000 bp.
+// Return: The reverse complement of s.
+
 def revc(dna: String): String = 
     dna.reverse.map(b => b match {
         case 'A' => 'T'
