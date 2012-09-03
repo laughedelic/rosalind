22ff144e60bd30a0bf4b3898b7be5335ef8847f8
Alexey Alekhin <alexey.alekhin@me.com>
2012-09-04 03:59:54 +0400

    grph problem solution

--- rosalind-solutions.scala
+++ rosalind-solutions.scala
@@ -2,6 +2,8 @@
 
 // Common functions:
 
+import scala.io.Source
+
 def updMap[K,V](m: Map[K,V], key: K, f: V => V): Map[K,V] =
     if(m contains key) m + (key -> f(m(key))) else m
 
@@ -60,4 +62,41 @@ def rna(dna: String): String =
 		case 'T' => 'U'
 		case  x  =>  x
 	})
+////////////////////////////////////////////////////////////////////////////////
+
+
+//// Hamming Distance: http://rosalind.info/problems/hamm/ /////////////////////
+// Given: Two DNA strings s and t of equal length (not exceeding 1 kbp).
+// Return: The Hamming distance dH(s,t) (number of symbols that differ).
+
+def hamm(s: String, t: String): Int = (s zip t) count ( x => x._1 != x._2 )
+////////////////////////////////////////////////////////////////////////////////
+
+
+//// Introduction to Graphs: http://rosalind.info/problems/grph/ ///////////////
+// Given: A collection of DNA strings in FASTA format.
+// Return: The adjacency list corresponding to the overlap graph for k=3.
+
+def parseGraph(s: String): Map[String, String] =
+	s.split('>').tail.foldLeft( Map[String, String]() )( (acc,x) => {
+			val y = x.split("\\s+")
+			acc + (y.head -> y.tail.mkString) 
+		})
+
+def hasOverlap(s1: String, s2: String, k: Int): Boolean =
+	(s1 takeRight k) == (s2 take k)
+
+def overlaps(g: Map[String, String], k: Int): List[(String, String)] =
+	g.foldLeft( List[(String, String)]() )( (accum, foo) => {
+			accum ++ (g.foldLeft( List[(String, String)]() )( (acc, bar) => {
+					if(foo._1 != bar._1 && hasOverlap(foo._2, bar._2, k))
+						(foo._1, bar._1) :: acc
+					else acc
+				}))
+		})
+
+def grph(s: String, k: Int): String =
+	overlaps(parseGraph(s), k).map( x => x._1 +" "+ x._2).mkString("\n")
+
+def grph_(file: String) = println(grph(Source.fromFile(file).mkString, 3))
 ////////////////////////////////////////////////////////////////////////////////
