e4f03fa7162f880759e1529eb43e5d863911079b
laughedelic <laughedelic@gmail.com>
2012-09-17 01:23:35 +0400

    prot problem solution

--- rosalind-solutions.scala
+++ rosalind-solutions.scala
@@ -100,3 +100,37 @@ def grph(s: String, k: Int): String =
 
 def grph_(file: String) = println(grph(Source.fromFile(file).mkString, 3))
 ////////////////////////////////////////////////////////////////////////////////
+
+
+//// Protein Translation: http://rosalind.info/problems/prot/ //////////////////
+// Given: An RNA string s corresponding to a strand of mRNA.
+// Return: The protein string encoded by s.
+
+val codonTable = Map(
+	 "UUU" -> "F"    ,"CUU" -> "L"    ,"AUU" -> "I"    ,"GUU" -> "V"
+	,"UUC" -> "F"    ,"CUC" -> "L"    ,"AUC" -> "I"    ,"GUC" -> "V"
+	,"UUA" -> "L"    ,"CUA" -> "L"    ,"AUA" -> "I"    ,"GUA" -> "V"
+	,"UUG" -> "L"    ,"CUG" -> "L"    ,"AUG" -> "M"    ,"GUG" -> "V"
+	,"UCU" -> "S"    ,"CCU" -> "P"    ,"ACU" -> "T"    ,"GCU" -> "A"
+	,"UCC" -> "S"    ,"CCC" -> "P"    ,"ACC" -> "T"    ,"GCC" -> "A"
+	,"UCA" -> "S"    ,"CCA" -> "P"    ,"ACA" -> "T"    ,"GCA" -> "A"
+	,"UCG" -> "S"    ,"CCG" -> "P"    ,"ACG" -> "T"    ,"GCG" -> "A"
+	,"UAU" -> "Y"    ,"CAU" -> "H"    ,"AAU" -> "N"    ,"GAU" -> "D"
+	,"UAC" -> "Y"    ,"CAC" -> "H"    ,"AAC" -> "N"    ,"GAC" -> "D"
+	,"UAA" -> "Stop" ,"CAA" -> "Q"    ,"AAA" -> "K"    ,"GAA" -> "E"
+	,"UAG" -> "Stop" ,"CAG" -> "Q"    ,"AAG" -> "K"    ,"GAG" -> "E"
+	,"UGU" -> "C"    ,"CGU" -> "R"    ,"AGU" -> "S"    ,"GGU" -> "G"
+	,"UGC" -> "C"    ,"CGC" -> "R"    ,"AGC" -> "S"    ,"GGC" -> "G"
+	,"UGA" -> "Stop" ,"CGA" -> "R"    ,"AGA" -> "R"    ,"GGA" -> "G"
+	,"UGG" -> "W"    ,"CGG" -> "R"    ,"AGG" -> "R"    ,"GGG" -> "G")
+
+def prot(rna: String): String = {
+	rna splitAt 3 match {
+		case (codon, rest) => 
+		codonTable(codon) match {
+			case "Stop" => ""
+			case 	x   => x + prot(rest)
+		}
+	}
+}
+////////////////////////////////////////////////////////////////////////////////
